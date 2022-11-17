d34623da0e9ade9c5b3cf1fca01b231e2aa8ced2
Ninad Sutrave <ninadsutrave@gmail.com>
2022-11-17 10:11:54 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className="Footer">
-        Apache-2.0 © <a className="github" href="https://github.com/NinadSutrave" target="_blank" rel="noreferrer"> Ninad Sutrave </a>
+        License © Apache-2.0 <a className="github" href="https://github.com/NinadSutrave" target="_blank" rel="noreferrer"> </a>
     </div>
   )
 }
